2d296427b2e35ecdced2e38b2054e2e04d0b7f97
KIRtony <ts15@iu.edu>
2020-09-28 03:29:57 -0500

    k

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -1,16 +1,21 @@
 extends Control
 
+onready var global = get_node("/root/Global")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	update_score(0)
+	update_health(0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func update_score(s):
+	global.score += s
+	$Score.text = "Score: " + str(global.score)
+	if global.score >= 100 and global.level != 2:
+		get_node("/root/Game/Level").show()
+		get_node("/root/Game/Level").monitoring = true
+
+func update_health(h):
+	global.health += h
+	$Health.text = "Health: " + str(global.health)
+	if global.health <= 0:
+		get_tree().change_scene("res://Menu/Die.tscn")
